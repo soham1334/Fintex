102cec3dbd84d36f448cc3170923a720bc12fb76
soham1334 <sohamhanmane57@gmail.com>
2025-06-05 00:16:24 +0530

    Update settings.py

--- Backend/finback/settings.py
+++ Backend/finback/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-    'learn',
     'rest_framework',
     'corsheaders',
     'Newssmry',
